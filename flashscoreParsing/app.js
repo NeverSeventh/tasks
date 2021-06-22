eb4131d2f77b430c6cbdeb001856ebfe093511ee
Ilya <logvinovIl2000@gmail.com>
2021-06-22 11:32:13 +0300

    Added dynamic parsing through puppeteer (First version)

--- flashscoreParsing/app.js
+++ flashscoreParsing/app.js
@@ -4,7 +4,7 @@ const logger = require('morgan');
 const Team = require('./models/team.model')
 const got = require('got');
 const cheerio = require('cheerio');
-
+const puppeteer = require('puppeteer')
 
 
 
@@ -15,24 +15,40 @@ app.use(logger('dev'));
 
 (async () => {
 	try {
-		const response = await got('https://www.flashscore.ru/team/chelsea/4fGZN2oK/');
-		const $ = cheerio.load(response.body);
+        const browser = await puppeteer.launch();
+        const page = await browser.newPage();
+        await page.goto('https://www.flashscore.ru/team/chelsea/4fGZN2oK/')
+  
+        let c = await page.evaluate(()=> {
+            let score = document.body.innerHTML;
+            return score;
+        })
+        
+		//const response = await got('https://www.flashscore.ru/team/chelsea/4fGZN2oK/');
+        
+		// const $ = cheerio.load(response.body)
+            const $ =  cheerio.load(c)
+        
+            const name = $('body').find('.teamHeader__name').text();
+            const logoStyle = $('body').find('.teamHeader__logo').attr('style');
+            
+            const logoUrl = logoStyle.slice(logoStyle.indexOf('/'),-1);
+            const countryEl = $('body').find('.breadcrumb__link');
+            //const country = countryEl.toArray()[1].children[0].data;
+            const country =  countryEl.eq(1).text();
+            //const res = await Team.insertTeam(name,country,logoUrl);
+            await browser.close();
+        
+ 
+       
 
-        const name = $('body').find('.teamHeader__name').text();
-        const logoStyle = $('body').find('.teamHeader__logo').attr('style');
-        
-        const logoUrl = logoStyle.slice(logoStyle.indexOf('/'),-1);
-        const countryEl = $('body').find('.breadcrumb__link');
-        //const country = countryEl.toArray()[1].children[0].data;
-        const country =  countryEl.eq(1).text();
-        const res = await Team.insertTeam(name,country,logoUrl);
-        console.log(res);
-        
+
 	} catch (error) {
 		console.log(error.message);
-		
+        
 	}
 })()
+
 
 app.listen(6000,()=> {
     console.log('server started');
--- flashscoreParsing/models/team.model.js
+++ flashscoreParsing/models/team.model.js
@@ -17,4 +17,6 @@ class Team {
     }
 }
 
+
+
 module.exports = Team;
