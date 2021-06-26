65be0afd1ba2aac8ccf158470bc0189c3b2a0235
Ilya <logvinovIl2000@gmail.com>
2021-06-26 17:59:47 +0300

    0.2.1

--- flashscoreParsing/app.js
+++ flashscoreParsing/app.js
@@ -1,6 +1,5 @@
 const got = require('got');
 const cheerio = require('cheerio');
-// const puppeteer = require('puppeteer');
 const Team = require('./models/team.model');
 
 (async () => {
@@ -12,22 +11,13 @@ const Team = require('./models/team.model');
       return $(parent).find(selector);
     }
 
-    // const browser = await puppeteer.launch();
-    // const page = await browser.newPage();
-    // await page.goto('https://www.flashscore.ru/team/chelsea/4fGZN2oK/')
-    // let c = await page.evaluate(()=> {
-    //     let score = document.body.innerHTML;
-    //     return score;
-    // })
-    // const $ =  cheerio.load(c)
-    const name = select('.teamHeader__name').text();
+    const name = select('.teamHeader__name').text().trim();
     const logoStyle = select('.teamHeader__logo').attr('style');
     const logoUrl = logoStyle.slice(logoStyle.indexOf('/'), -1);
     const country = select('.breadcrumb__link').eq(1).text();
 
     const res = await Team.insertTeam(name, country, logoUrl);
     console.log(res);
-    // await browser.close();
   } catch (error) {
     console.log(error.message);
   }
--- flashscoreParsing/models/team.model.js
+++ flashscoreParsing/models/team.model.js
@@ -5,11 +5,11 @@ const db = getDb.getDb();
 class Team {
   static async insertTeam(name, country, logo) {
     try {
-      const team = await db.query('select id from team where name=?', [name]);
+      if (typeof country !== 'string' || typeof name !== 'string' || typeof logo !== 'string') throw new Error('Неправильный формат данных');
 
-      if (!team[0]) {
-        if (typeof country !== 'string' || typeof name !== 'string' || typeof logo !== 'string') throw new Error('Неправильный формат данных');
+      const [id] = await db.query('select id from team where name=?', [name]);
 
+      if (!id) {
         const response = await db.query('insert into team (name,country,logo) values (?,?,?)', [name, country, logo]);
 
         return response.insertId;
